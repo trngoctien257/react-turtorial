3962d37bc51bd4a0485abee3afa3cd1963007092
tien <tientrinh257@bitbucket.org>
2018-10-18 18:09:29 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
+import Table from './Table.js';
+import Form from './Form.js';
+import Api from './Api.js';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		characters : []
+	};
+
+	removeCharacter = index => {
+		const { characters } = this.state;
+
+		this.setState({
+			characters : characters.filter((character, i) => {
+				return i !== index;
+			})
+		});
+	}
+
+	handleSubmit = character => {
+		this.setState({characters : [...this.state.characters , character] });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
+				<Form handleSubmit={this.handleSubmit} />
+				<Api />
+			</div>
+		);
+	}
 }
 
 export default App;
